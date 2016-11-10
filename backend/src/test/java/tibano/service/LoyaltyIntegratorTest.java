ecc840f6db0de1362001234f0b1d242a5edbfd7f
maas <maas@skidata.com>
2016-11-10 16:33:23 +0100

    Call Loyalty asynchronous.

--- backend/src/test/java/tibano/service/LoyaltyIntegratorTest.java
+++ backend/src/test/java/tibano/service/LoyaltyIntegratorTest.java
@@ -1,12 +1,18 @@
 package tibano.service;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class LoyaltyIntegratorTest {
 
 	@Test
-	public void addPoints() {
-		Assert.assertTrue(new LoyaltyIntegrator().addPoints(0));
+	public void addPoints() throws InterruptedException, ExecutionException {
+		Future<Boolean> result = new LoyaltyIntegrator().addPoints(0);
+		while(!result.isDone())
+			Thread.sleep(10); //10-millisecond pause between each check
+		Assert.assertTrue(result.get());
 	}
 }
