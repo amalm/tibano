17843388e6e0ed85724ca66361bb98bbd6fd623a
maas <maas@skidata.com>
2016-11-10 12:24:33 +0100

    Map view implemented.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,12 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
+    'uiGmapgoogle-maps',
     'myApp.map',
     'myApp.start',
     'myApp.stop',
     'myApp.enforcement'
-]).
-config(['$locationProvider', '$routeProvider', '$httpProvider',
+]).config(['$locationProvider', '$routeProvider', '$httpProvider',
 function($locationProvider, $routeProvider, $httpProvider) {
     $locationProvider.hashPrefix('!');
 
@@ -19,6 +19,13 @@ function($locationProvider, $routeProvider, $httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }])
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      //key: 'your api key',
+      //libraries: 'weather,geometry,visualization',
+      v: '3.17'
+    });
+  })
 .run(function($rootScope) {
     $rootScope.currentUser = { id:1, name:"Hubert Farnsworth"};
 });
