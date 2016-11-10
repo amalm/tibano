101f4a08eb01f52be5317a7576bd2867bcf298a7
Thomas Ries <thomas.ries@skidata.com>
2016-11-10 15:09:33 +0100

    change ip in baseUrl

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -30,5 +30,5 @@ function($locationProvider, $routeProvider, $httpProvider) {
 .run(function($rootScope, $http) {
     $rootScope.currentUser = { id:1, name:"Hubert Farnsworth"};
     $rootScope.selectedArea = { id:1, name:"Airport"};
-    $rootScope.basisUrl = 'http://192.168.0.101:8080';
+    $rootScope.basisUrl = 'http://192.168.1.101:8080';
 });
