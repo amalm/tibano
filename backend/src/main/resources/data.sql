02340ea44b8055995c5e7e77ae177aff91ab652c
Thomas Ries <thomas.ries@skidata.com>
2016-11-10 11:36:51 +0100

    latitude and longitude for areas

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into Area(id, name, capacity, occupied) values(1, 'Airport', 500, 0);
-insert into Area(id, name, capacity, occupied) values(2, 'Central Station', 40, 0);
-insert into Area(id, name, capacity, occupied) values(3, 'Park&Ride south', 130, 0);
+insert into Area(id, name, capacity, occupied, latitude, longitude) values(1, 'Airport', 500, 0, 47.829411, 13.005432);
+insert into Area(id, name, capacity, occupied, latitude, longitude) values(2, 'Central Station', 40, 0, 47.813777, 13.044259);
+insert into Area(id, name, capacity, occupied, latitude, longitude) values(3, 'Park&Ride south', 130, 0, 47.769629,	13.07166);
 
 insert into User(id, name) values(1, 'Hubert Farnsworth');
 
@@ -8,3 +8,6 @@ insert into Car(id, license_plate, user_id) values (1, 'HH FT 4711', 1);
 insert into Car(id, license_plate, user_id) values (2, 'K 361 GO', 1);
 insert into Car(id, license_plate, user_id) values (3, 'S 123 AB', 1);
 insert into Car(id, license_plate, user_id) values (4, 'SL863MG', 1);
+
+
+
